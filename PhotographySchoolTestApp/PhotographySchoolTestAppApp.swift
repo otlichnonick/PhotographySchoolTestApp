e85a63e81401aa9ff80d2324d1d8eceeeab68aab
Anton Agafonov <otlichnonick@yandex.ru>
2023-03-30 08:02:09 +0200

    Initial Commit

---
+++ PhotographySchoolTestApp/PhotographySchoolTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotographySchoolTestAppApp.swift
+//  PhotographySchoolTestApp
+//
+//  Created by Anton Agafonov on 30.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct PhotographySchoolTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
